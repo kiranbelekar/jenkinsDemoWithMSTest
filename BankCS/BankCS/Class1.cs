3b3ab7b03661a1e69dfc7b7fbe1359c09c4b4bff
Kiran Belekar <kiran.belekar@acellere.com>
2020-04-01 13:05:17 +0530

    my first commit

---
+++ BankCS/BankCS/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace BankAccountNS
+{
+    /// <summary> 
+    /// Bank Account demo class. 
+    /// </summary> 
+    public class BankAccount
+    {
+
+        private bool frozenValue = false;
+
+        private BankAccount()
+        {
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            CustomerName = customerName;
+            Balance = balance;
+        }
+
+        public string CustomerName { get; private set; }
+
+
+        public double Balance { get; private set; }
+
+        public void Debit(double amount)
+        {
+            if (frozenValue)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            Balance += amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (frozenValue)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount > Balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            Balance += amount;
+        }
+
+        private void FreezeAccount()
+        {
+            frozenValue = true;
+        }
+
+        private void UnfreezeAccount()
+        {
+            frozenValue = false;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+
+    }
+}
+
+/* The example companies, organizations, products, domain names, e-mail addresses, logos, people, places, and events depicted herein are fictitious.  No association with any real company, organization, product, domain name, email address, logo, person, places, or events is intended or should be inferred. */
---
+++ BankCS/BankCSTests1/BankAccountTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountNS.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void DebitTest()
+        {
+            BankAccount target = new BankAccount("Mr. Bryan Walton", 11.99);
+            double amount = 11.22;
+            Assert.Fail();
+        }
+    }
+}
